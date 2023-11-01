4f5f459f02686f246c0477c423e98b7a6dcb35a2
Warze <contact@warze.org>
2023-11-01 19:30:51 +0100

    Added comments and refactored code

--- src/dom.ts
+++ src/dom.ts
@@ -1,17 +1,25 @@
 export function goTo(page_id: string): void {
+	// Hide all previously selected pages and nav elements
 	document.querySelectorAll('.navitem.selected, .page.selected').forEach((element) => {
 		element.classList.remove('selected');
 	});
 
-	document.querySelector<HTMLElement>(`.navitem#nav${page_id}`)?.classList.add('selected');
-	document.querySelector<HTMLElement>(`.page#${page_id}`)?.classList.add('selected');
+	// Select only the applicable nav element and page
+	document.querySelectorAll(`.navitem#nav${page_id}, .page#${page_id}`).forEach((element) => {
+		element.classList.add('selected');
+	});
 }
 
 window.onload = () => {
+	// See what page the user loaded the page on
 	const hash = window.location.hash;
+	
 	if (['#home','#',''].includes(hash)) {
+		// Don't change any pages, just select the home in the nav
 		document.getElementById('navhome')?.classList.add('selected');
 		return;
 	}
+
+	// Change page immediately
 	goTo(hash.replace('#', ''));
 }
